c3908681eed779aa585e421907c56a3f0c582e78
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2025-03-13 07:19:11 +0545

    push

--- DivisibleBy7.cs
+++ DivisibleBy7.cs
@@ -4,14 +4,14 @@ class DivisibleBy7{
     static void Main(){
         Console.WriteLine("Enter the first number:");
         int a = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Enter the second number:");
-        int b = Convert.ToInt32(Console.ReadLine());
+        // Console.WriteLine("Enter the second number:");
+        // int b = Convert.ToInt32(Console.ReadLine());
 
-        if(a/b==7){
-            Console.WriteLine("This number is divisible by 7");
+        if(a/7==0){
+            Console.WriteLine("This number is not divisible by 7");
 
         }else{
-            Console.WriteLine("This number is not divisible by 7");
+            Console.WriteLine("This number is  divisible by 7");
         }
     }
 }
