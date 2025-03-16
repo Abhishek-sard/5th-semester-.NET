473dd80208e06f669b45a6540d6c249dcaf4c06a
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2025-03-16 15:06:19 +0545

    push

---
+++ VowelLetters.cs
@@ -0,0 +1,13 @@
+using System;
+
+class VowelLetter
+{
+    static void Main()
+    {
+        // Define an array to store vowels
+        char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+
+        // Print only 'o'
+        Console.WriteLine("The vowel 'o' is: " + vowels[3]);
+    }
+}
