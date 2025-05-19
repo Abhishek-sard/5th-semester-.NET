10fbecf05fb0f558fb316ab8cf7e0a39c2dc8ec3
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2025-05-19 21:02:23 +0545

    go

---
+++ note.cs
@@ -0,0 +1,7 @@
+//define lambda expression
+//A way to define an anonymous function or method without explicity creating a method or delegate.
+//explain anonymous method with example.
+//An anonymous method is a method that does not have a name and is defined inline.
+//It is often used in event handling or as a callback function.
+//Anonymous methods are useful when you need to pass a method as a parameter to another method.
+
